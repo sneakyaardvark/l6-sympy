56ead5c3b8b19436a61446892a0783e6605644a3
Sneaky Aardvark <81195290+sneakyaardvark@users.noreply.github.com>
2023-03-08 17:55:05 -0800

    progress on 2 and plotting for 1

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sympy
 from math import degrees
+
 from sympy.physics.control.lti import TransferFunction
 from sympy.physics.control import control_plots as cplot
-
+from numpy import linspace
 
 def problem_1a():
     x, y, t1, t2 = sympy.symbols("x, y, t1, t2")
@@ -18,6 +19,14 @@ def problem_1a():
         )
 
 
+def problem_1b2():
+    x, y, t1, t2 = sympy.symbols("x, y, t1, t2")
+    fx = 4 * sympy.cos(t1) + 3 * sympy.cos(t2) - 6
+    fy = 4 * sympy.sin(t1) + 3 * sympy.sin(t2) - y
+    # ans = [sympy.solve([fx, fy.subs({y: i})], (t1, t2)) for i in linspace(0.1, 3.6)]
+    sympy.pprint(sympy.solve([fx, fy.subs({y: 2})], (t1, t2)))
+
+
 def problem_1b():
     x, y, t1, t2 = sympy.symbols("x, y, t1, t2")
     fx = 4 * sympy.cos(t1) + 3 * sympy.cos(t2) - 6
@@ -26,15 +35,21 @@ def problem_1b():
     ft1 = sympy.solve(fx, t1, dict=True)
     ft2 = sympy.solve(fx, t2, dict=True)
 
+    sympy.pprint(ft1)
+    sympy.pprint(ft2)
+
     # f1 = sympy.solve([ft, fy], t2)
     f1 = fy.subs(ft1[1])
     a1 = sympy.solve(f1, t2)
     f2 = fy.subs(ft2[1])
     a2 = sympy.solve(f2, t1)
 
-    # p = sympy.plot(a2[1], show=False)
-    # p.show()
-    sympy.pprint([a1[0], a2[0]])
+    # sympy.pprint([a1, a2])
+
+    plt_theta1 = sympy.plot(a1[0], (y, 0.1, 3.6), show=False)
+    plt_theta2 = sympy.plot(a2[0], (y, 0.1, 3.6), show=False)
+    sympy.plotting.PlotGrid(2, 1, plt_theta1, plt_theta2)
+    # sympy.pprint([a1[0], a2[0]])
 
 
 def example_2():
@@ -49,23 +64,55 @@ def example_2():
 
 
 def problem_2b():
-    def magnitude(function):
-        return sympy.sqrt(sympy.re(function)**2 + sympy.im(function)**2)
-
     r, s, c, l, w = sympy.symbols("r, s, c, l, w", real=True)
-    n = r * s * c
-    d = (s**2 * l * c + r * c * s + 1)
-    n = n.subs({s: sympy.I * w})
-    d = d.subs({s: sympy.I * w})
-    n_mag = sympy.Abs(n)
-    d_mag = sympy.Abs(d)
-    h = TransferFunction(n_mag.collect(w), d_mag.collect(w), w)
-    sympy.pprint(h)
+    n = (r*w)/l
+    d = sympy.sqrt((1 / (l * c) - w**2)**2 + (r*w/l)**2)
+    h = n / d
+    values = {
+        c: 10e-6,
+        l: 5e-3,
+    }
+    w_range = (w, 100, 100000)
+    h = h.subs(values)
+    h_r10 = h.subs({r: 10})
+    h_r100 = h.subs({r: 100})
+    h_r1k = h.subs({r: 1000})
+    plt = sympy.plot(h_r10, w_range, show=False, legend=True, label="R=10")
+    plt_r100 = sympy.plot(h_r100, w_range, show=False, label="R=100")
+    plt_r1k = sympy.plot(h_r1k, w_range, show=False, label="R=1k")
+    plt.extend(plt_r100)
+    plt.extend(plt_r1k)
+    plt.title = r"Transfer Function $\it{H}$"
+    plt.xlabel = r"$\it{\omega}$ (rad/s)"
+    plt.ylabel = r"$\it{H}(\omega)$"
+    plt.show()
 
 
 def problem_2c():
-    pass
+    r, s, c, l, w = sympy.symbols("r, s, c, l, w", real=True)
+    n = (r * w) / l
+    d = sympy.sqrt((1 / (l * c) - w ** 2) ** 2 + (r * w / l) ** 2)
+    h = TransferFunction(n, d, w)
+    values = {
+        c: 10e-6,
+        l: 5e-3,
+    }
+    h = h.subs(values)
+    r_values = [10, 100, 1000]
+    for i in r_values:
+        f = h.subs({r: i})
+        cplot.bode_magnitude_plot(f)
+        # bp = cplot.impulse_response_plot(f, show=False)
+        # bp.show()
+
+        # part d
+        # cplot.impulse_response_plot(f)
+
+
+def problem_2e():
+    v_r, t, r, l, c, v_i = sympy.symbols("v_r, t, r, l, c, v_i")
+    diff = sympy.Derivative()
 
 
 if __name__ == "__main__":
-    problem_2b()
+    problem_1b2()
